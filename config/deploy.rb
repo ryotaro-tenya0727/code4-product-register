d3acd49483d8b05fd9f7841153545d5cd749ddb9
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2021-08-30 20:01:11 +0900

    caspi config

---
+++ config/deploy.rb
@@ -0,0 +1,36 @@
+lock '3.16.0'
+
+# アプリケーションの指定
+set :application, 'product_register_new'
+set :repo_url,  'git@github.com:ryotaro-tenya0727/product_register_new.git'
+
+# sharedディレクトリに入れるファイルを指定
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
+
+# SSH接続設定
+set :ssh_options, {
+  auth_methods: ['publickey'],
+  keys: ['~/desktop/product.pem']
+}
+
+# 保存しておく世代の設定
+set :keep_releases, 5
+
+# rbenvの設定
+set :rbenv_type, :user
+set :rbenv_ruby, '2.5.9'
+
+# ここからUnicornの設定
+# Unicornのプロセスの指定
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+
+# Unicornの設定ファイルの指定
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+
+# Unicornを再起動するための記述
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
---
+++ config/deploy/production.rb
@@ -0,0 +1,1 @@
+server '35.76.61.194', user: 'ec2-user', roles: %w{app db web}
