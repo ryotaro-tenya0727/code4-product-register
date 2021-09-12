a201d4d66549bd09d9634c727afd0bb5de0d8989
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2021-09-12 12:53:16 +0900

    appspecを編集 codecommit にpush 可能

---
+++ scripts/bundle_install.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cd /var/www/code4-product-register && bundle install --path vendor/bundle
---
+++ scripts/start_server.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cd /var/www/code4-product-register && unicorn_rails -c config/unicorn.rb -E production -D
