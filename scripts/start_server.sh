b24647a963bb741cedc461f8d18b454c98209b13
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2021-09-13 19:12:54 +0900

    remove asset

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd /var/www/code4-product-register
+rails assets:precompile RAILS_ENV=production
 rails db:create RAILS_ENV=production
 rails db:migrate RAILS_ENV=production
 unicorn_rails -c config/unicorn.rb -E production -D
