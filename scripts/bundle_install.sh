67037837c073d059714733b65dc73edddd7b3d85
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2021-09-14 00:19:53 +0900

    gem install

--- scripts/bundle_install.sh
+++ scripts/bundle_install.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-cd /var/www/code4-product-register && bundle install --path vendor/bundle
+cd /var/www/code4-product-register
+gem install bundler -v 1.17.3
+bundle install --path vendor/bundle
