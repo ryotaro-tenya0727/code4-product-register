7b9ec44ce5267283f95c04a7f0679c4582769232
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2021-09-14 11:43:39 +0900

    fullpath

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,4 +3,4 @@ source ~/.bash_profile
 cd /var/www/code4-product-register
 sudo systemctl reload nginx
 sudo systemctl start nginx
-bundle exec unicorn -D -E production -c config/unicorn.rb
+/home/ec2-user/bin/bundle exec unicorn -D -E production -c config/unicorn.rb
