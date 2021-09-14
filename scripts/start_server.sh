0b1a4d854f204c4ac6a80ed7dc8ca6a97c119377
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2021-09-14 17:17:30 +0900

    実験

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 source ~/.bash_profile
 cd /var/www/code4-product-register
-sudo systemctl reload nginx
-sudo systemctl start nginx
 bundle exec unicorn -D -E production -c config/unicorn.rb
